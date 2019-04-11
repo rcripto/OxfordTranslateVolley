ec3f13f7e729993c19f20b150ea0dad7681e3f41
Rodrigo Gazola <rodrigogazola@tokenlab.com.br>
2019-04-11 19:36:57 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/oxtranslate/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.oxtranslate
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "df663feb" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "dde2e7c067d3210ff419fdf04450eaf2" // Preeencher com seu app_key
+}
